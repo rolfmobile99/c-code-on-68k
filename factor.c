1d276a4e4b015b607034e65603349fa39ae43870
Rolf Widenfelt <rolfvw@gmail.com>
2021-04-05 23:06:59 -0700

    add credits

--- factor.c
+++ factor.c
@@ -1,11 +1,18 @@
 /*
-  Example of compiling C code for 68000. Doesn't require any C library
-  or other run-time code except libgcc. Relies on the Motorola TUTOR
-  monitor program.
+  factor.c - calculate and print factorials up to 13!
+  
+  This code is the starting point for running a compiled C program on the 68k.
+  
+  Note: code is loosely based on the factorial demo from Jeff Tranter:
+    http://jefftranter.blogspot.com/2017/05/building-68000-single-board-computer-c.html
+  He also includes some really helpful hints on setting up gcc for the 68k.
 
-  3/22/2021 - simplified. now based on printf and putchar directly.  (rolf)
+  3/22/2021 - rolf
+  - simplified. now based on printf and putchar directly.  (rolf)
   - fixed typo in printf (this generates n^3, not n^4 as was stated).
   - increased loop to 13 (13! is about 1.9B, still within a signed 32-bit value)
+  
+  AUTHORS: Rolf W, Roderick M
 */
 
 #include <stdio.h>
